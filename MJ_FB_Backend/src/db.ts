9252d64ccc631fd7dba16a0d04107a3e572dac48
Amrutha A J <38122023+Amrutha-A-J@users.noreply.github.com>
2025-08-04 15:19:43 -0600

    refactor: connect to database during server startup

--- MJ_FB_Backend/src/db.ts
+++ MJ_FB_Backend/src/db.ts
@@ -9,10 +9,4 @@ const pool = new Pool({
   port: Number(process.env.PG_PORT),
   database: process.env.PG_DATABASE,
 });
-
-// Test the connection
-pool.connect()
-  .then(() => console.log('✅ Connected to the database successfully!'))
-  .catch(err => console.error('❌ Failed to connect to the database:', err));
-
 export default pool;
