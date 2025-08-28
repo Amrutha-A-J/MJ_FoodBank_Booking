174ea67244a22e7af31a4555dfe424f8ff16194b
Amrutha A J <38122023+Amrutha-A-J@users.noreply.github.com>
2025-08-28 17:43:40 -0600

    chore: add migration for visited booking status

--- MJ_FB_Backend/tests/bookingsStatusConstraint.test.ts
+++ MJ_FB_Backend/tests/bookingsStatusConstraint.test.ts
@@ -1,8 +1,8 @@
 import MigrationBuilder from 'node-pg-migrate/dist/migrationBuilder';
-import { up } from '../migrations/20241120120000_add_no_show_to_bookings';
+import { up } from '../migrations/20241201120000_add_visited_to_bookings';
 
 describe('bookings status constraint migration', () => {
-  it('includes no_show in allowed statuses', async () => {
+  it('includes visited in allowed statuses', async () => {
     const pgm = new (MigrationBuilder as any)({ log: () => {} });
     await up(pgm);
     const sql = pgm.getSql();
