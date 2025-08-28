0f5aa3a55fa83dc9df37e54bdfb50aa24596a3c9
Amrutha A J <38122023+Amrutha-A-J@users.noreply.github.com>
2025-08-28 11:37:29 -0600

    Add admin settings routes and navigation

---
+++ MJ_FB_Frontend/src/pages/admin/VolunteerSettings.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@mui/material';
+import Page from '../../components/Page';
+
+export default function VolunteerSettings() {
+  return (
+    <Page title="Volunteer Settings">
+      <Box p={2}></Box>
+    </Page>
+  );
+}
+
