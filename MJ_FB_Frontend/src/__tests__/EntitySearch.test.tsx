f7255ca1935538ff2991541f94d9dcba997c4e91
Amrutha A J <38122023+Amrutha-A-J@users.noreply.github.com>
2025-08-28 21:30:42 -0600

    Add test for clearing EntitySearch

---
+++ MJ_FB_Frontend/src/__tests__/EntitySearch.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import EntitySearch from '../components/EntitySearch';
+
+describe('EntitySearch', () => {
+  it('clears query after selection when clearOnSelect is true', async () => {
+    const searchFn = jest.fn().mockResolvedValue([{ id: 1, name: 'Client 1', client_id: 1 }]);
+    render(
+      <EntitySearch
+        type="user"
+        placeholder="Search clients"
+        onSelect={() => {}}
+        searchFn={searchFn}
+        clearOnSelect
+      />,
+    );
+
+    const input = screen.getByLabelText(/search/i);
+    fireEvent.change(input, { target: { value: 'Cli' } });
+
+    await waitFor(() => expect(searchFn).toHaveBeenCalled());
+
+    const button = await screen.findByRole('button', { name: /Client 1 \(1\)/ });
+    fireEvent.click(button);
+
+    expect((input as HTMLInputElement).value).toBe('');
+  });
+});
