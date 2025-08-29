73b5f4942f184b7f7b3fab2535359591473c9323
Amrutha A J <38122023+Amrutha-A-J@users.noreply.github.com>
2025-08-28 23:46:22 -0600

    feat: show volunteer charts side by side

--- MJ_FB_Frontend/src/pages/volunteer-management/VolunteerDashboard.tsx
+++ MJ_FB_Frontend/src/pages/volunteer-management/VolunteerDashboard.tsx
@@ -276,11 +276,20 @@ export default function VolunteerDashboard() {
             </SectionCard>
           </Grid>
         )}
-        {contributionData.length > 0 && (
+        {contributionData.length > 0 ? (
+          <>
+            <Grid size={{ xs: 12, md: 6 }}>
+              <SectionCard title="My Contribution Trend">
+                <PersonalContributionChart data={contributionData} />
+              </SectionCard>
+            </Grid>
+            <Grid size={{ xs: 12, md: 6 }}>
+              <VolunteerGroupStatsCard />
+            </Grid>
+          </>
+        ) : (
           <Grid size={{ xs: 12 }}>
-            <SectionCard title="My Contribution Trend">
-              <PersonalContributionChart data={contributionData} />
-            </SectionCard>
+            <VolunteerGroupStatsCard />
           </Grid>
         )}
         <Grid size={{ xs: 12, md: 6 }}>
@@ -417,10 +426,6 @@ export default function VolunteerDashboard() {
               </Button>
             </Stack>
           </SectionCard>
-        </Grid>
-
-        <Grid size={{ xs: 12, md: 6 }}>
-          <VolunteerGroupStatsCard />
         </Grid>
 
         <Grid size={{ xs: 12, md: 6 }}>
