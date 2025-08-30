3de3d958ce95f8fdcdf774ecc17fb7940847530e
Amrutha A J <38122023+Amrutha-A-J@users.noreply.github.com>
2025-08-30 10:51:20 -0600

    test: mock ResizeObserver and IntersectionObserver

--- MJ_FB_Frontend/jest.setup.ts
+++ MJ_FB_Frontend/jest.setup.ts
@@ -6,3 +6,18 @@ import { TextEncoder, TextDecoder } from 'util';
 (global as any).TextDecoder = TextDecoder as any;
 (Element.prototype as any).scrollIntoView = jest.fn();
 (global as any).VITE_API_BASE = 'http://localhost:4000';
+
+// Mock ResizeObserver and IntersectionObserver for testing environment
+class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+(global as any).ResizeObserver = ResizeObserver;
+
+class IntersectionObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+(global as any).IntersectionObserver = IntersectionObserver;
