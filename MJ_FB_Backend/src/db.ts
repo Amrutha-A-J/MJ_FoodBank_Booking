3091a98537966b2650b6180cc7f836ccc235217d
Amrutha <amruthaajadiyath@gmail.com>
2025-09-04 22:47:56 -0600

    db chnages

--- MJ_FB_Backend/src/db.ts
+++ MJ_FB_Backend/src/db.ts
@@ -1,6 +1,12 @@
 import { Pool } from 'pg';
+import fs from 'fs';
+import path from 'path';
 import config from './config';
 import logger from './utils/logger';
+
+const caPath =
+  process.env.PGSSLROOTCERT ||
+  path.resolve(process.cwd(), 'certs/rds-global-bundle.pem');
 
 const pool = new Pool({
   user: config.pgUser,
@@ -8,6 +14,11 @@ const pool = new Pool({
   host: config.pgHost,
   port: config.pgPort,
   database: config.pgDatabase,
+  ssl: {
+    ca: fs.readFileSync(caPath, 'utf8'),
+    rejectUnauthorized: true,
+    servername: config.pgHost, // ensure hostname matches cert
+  },
 });
 
 pool.on('error', (err) => logger.error('Unexpected PG pool error', err));
