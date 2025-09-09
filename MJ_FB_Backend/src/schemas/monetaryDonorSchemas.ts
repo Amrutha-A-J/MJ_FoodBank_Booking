953571380f6c2283beded7c598be5827269810df
Amrutha A J <38122023+Amrutha-A-J@users.noreply.github.com>
2025-09-09 09:25:41 -0600

    feat: implement monetary donor APIs

---
+++ MJ_FB_Backend/src/schemas/monetaryDonorSchemas.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+export const monetaryDonorSchema = z.object({
+  firstName: z.string().min(1),
+  lastName: z.string().min(1),
+  email: z.string().email(),
+});
+
+export const addMonetaryDonorSchema = monetaryDonorSchema;
+export const updateMonetaryDonorSchema = monetaryDonorSchema;
+
+export type MonetaryDonorSchema = z.infer<typeof monetaryDonorSchema>;
